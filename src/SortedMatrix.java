d598d65abb9a325911d0f8bf39edb5f005045756
ganesh <ganeshsadhashivam@outlook.com>
2023-03-18 23:21:15 +0530

    sorted

---
+++ src/SortedMatrix.java
@@ -0,0 +1,31 @@
+public class SortedMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    //search in the row provided betweens columns provided
+    static int[]binarySearch(int[][ ]matrix , int row,int cStart,int cEnd,int target){
+        while(cStart <= cEnd){
+            int mid = cStart + (cEnd - cStart)/2;
+            if(matrix[row][mid] == target){
+                return new int[]{row,mid};
+            }
+            if(matrix[row][mid] < target){
+                    cStart = mid +1;
+            }else{
+                cEnd = mid-1;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+    static int []search(int[][] matrix,int target){
+        int rows = matrix.length;
+        int cols = matrix[0].length;//matrix may be empty
+
+        if(rows == 1){
+            return binarySearch(matrix,0,0,cols-1,target);
+
+        }
+        //run the loop
+    }
+}
